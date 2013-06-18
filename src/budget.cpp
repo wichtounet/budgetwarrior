c53008dd2687ac7e694dcae3516767f446fed4df
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-06-18 18:28:53 +0200

    Skeleton of the main

--- src/budget.cpp
+++ src/budget.cpp
@@ -5,10 +5,46 @@
 //  http://www.boost.org/LICENSE_1_0.txt)
 //=======================================================================
 
+#include <string>
 #include <iostream>
 
 int main(int argc, const char* argv[]) {
-    std::cout << "In development" << std::endl;
+    if(argc == 1){
+        std::cout << "A command is necessary" << std::endl;
+
+        return 1;
+    }
+
+    std::string command(argv[1]);
+
+    if(command == "help"){
+        std::cout << "Usage: budget command [options]" << std::endl;
+
+        //TODO Display complete help
+    } else if(command == "debt"){
+        if(argc == 2){
+            std::cout << "List of debts" << std::endl;
+
+            //TODO Implement display
+        } else if(argc > 2){
+            std::string subcommand(argv[2]);
+
+            if(subcommand == "add"){
+                std::cout << "Add a new debt" << std::endl;
+
+                //TODO Implement creation of debts
+            } else {
+                std::cout << "Invalid subcommand \"" << subcommand << "\"" << std::endl;
+
+                return 1;
+            }
+
+        }
+    } else {
+        std::cout << "Invalid command \"" << command << "\"" << std::endl;
+
+        return 1;
+    }
 
     return 0;
 }
