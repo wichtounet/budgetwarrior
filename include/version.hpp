a2dd29a071bfb108e9d22b792728e1babbd180be
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-06-21 14:11:43 +0200

    Include version command

---
+++ include/version.hpp
@@ -0,0 +1,30 @@
+//=======================================================================
+// Copyright (c) 2013-2014 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#ifndef BUDGET_VERSION_H
+#define BUDGET_VERSION_H
+
+#include <vector>
+#include <string>
+
+#include "module_traits.hpp"
+
+namespace budget {
+
+struct version_module {
+     void handle(const std::vector<std::string>& args);
+};
+
+template<>
+struct module_traits<version_module> {
+    static constexpr const bool is_default = false;
+    static constexpr const char* command = "version";
+};
+
+} //end of namespace budget
+
+#endif
