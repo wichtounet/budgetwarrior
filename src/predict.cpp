baeada3635c22e0ba0fe1b24e2f905026798ef0d
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-07-21 13:30:11 +0200

    First draft of predict

--- src/predict.cpp
+++ src/predict.cpp
@@ -5,6 +5,7 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
+#include <map>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -23,7 +24,51 @@ using namespace budget;
 namespace {
 
 void predict_overview(){
+    auto today = budget::local_day();
 
+    auto& expenses = all_expenses();
+    auto& earnings = all_earnings();
+
+    auto accounts = all_accounts(today.year(), today.month());
+
+    std::map<std::size_t, std::size_t> account_mappings;
+
+    std::vector<double> expense_multipliers(accounts.size(), 100.0);
+    std::vector<double> earning_multipliers(accounts.size(), 100.0);
+
+    std::cout << "Multipliers for expenses" << std::endl;
+
+    std::size_t i = 0;
+    for(auto& account : accounts){
+        account_mappings[account.id] = i;
+
+        std::cout << "   ";
+        edit_double(expense_multipliers[i], account.name);
+
+        ++i;
+    }
+
+    for(auto& expense : expenses){
+        if(account_mappings.count(expense.account)){
+            expense.amount *= (expense_multipliers[account_mappings[expense.account]] / 100.0);
+        }
+    }
+
+    for(auto& earning : earnings){
+        if(account_mappings.count(earning.account)){
+            earning.amount *= (earning_multipliers[account_mappings[earning.account]] / 100.0);
+        }
+    }
+
+    std::cout << std::endl;
+    display_local_balance(today.year(), false);
+    std::cout << std::endl;
+    display_balance(today.year(), false);
+    std::cout << std::endl;
+    display_expenses(today.year(), false);
+    std::cout << std::endl;
+    display_earnings(today.year(), false);
+    std::cout << std::endl;
 }
 
 } // end of anonymous namespace
