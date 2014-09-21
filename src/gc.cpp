145b74345575c3f6aceb80a1501a4c0165aeb47f
Baptiste Wicht <baptiste.wicht@gmail.com>
2014-09-21 16:43:31 +0200

    GC all common data

--- src/gc.cpp
+++ src/gc.cpp
@@ -11,8 +11,12 @@
 
 #include "gc.hpp"
 #include "accounts.hpp"
-#include "expenses.hpp"
+#include "debts.hpp"
 #include "earnings.hpp"
+#include "expenses.hpp"
+#include "fortune.hpp"
+#include "wishes.hpp"
+#include "objectives.hpp"
 
 using namespace budget;
 
@@ -44,18 +48,50 @@ void gc_earnings(){
     set_earnings_changed();
 }
 
+void gc_debts(){
+    auto next_id = gc(all_debts());
+    set_debts_next_id(next_id);
+    set_debts_changed();
+}
+
+void gc_fortunes(){
+    auto next_id = gc(all_fortunes());
+    set_fortunes_next_id(next_id);
+    set_fortunes_changed();
+}
+
+void gc_wishes(){
+    auto next_id = gc(all_wishes());
+    set_wishes_next_id(next_id);
+    set_wishes_changed();
+}
+
+void gc_objectives(){
+    auto next_id = gc(all_objectives());
+    set_objectives_next_id(next_id);
+    set_objectives_changed();
+}
+
 } //end of anonymous namespace
 
 void budget::gc_module::load(){
     load_accounts();
     load_expenses();
     load_earnings();
+    load_debts();
+    load_fortunes();
+    load_wishes();
+    load_objectives();
 }
 
 void budget::gc_module::unload(){
     save_expenses();
     save_earnings();
     save_accounts();
+    save_debts();
+    save_fortunes();
+    save_wishes();
+    save_objectives();
 }
 
 void budget::gc_module::handle(const std::vector<std::string>& args){
@@ -66,6 +102,10 @@ void budget::gc_module::handle(const std::vector<std::string>& args){
 
         gc_expenses();
         gc_earnings();
+        gc_debts();
+        gc_fortunes();
+        gc_wishes();
+        gc_objectives();
 
         std::cout << "...done" << std::endl;
     }
