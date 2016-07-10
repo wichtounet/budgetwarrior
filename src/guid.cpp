8e3e144a1a94730dd0da7463803b61eaaf28e0be
Adam Sowa <strixcode@gmail.com>
2016-07-10 07:52:15 +0200

    Windows platform support

--- src/guid.cpp
+++ src/guid.cpp
@@ -5,16 +5,30 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
+#ifdef _WIN32
+#include <windows.h>
+#pragma comment(lib, "rpcrt4.lib")  // UuidCreate - Minimum supported OS Win 2000
+#else
 #include <uuid/uuid.h>
+#endif
 
 #include "guid.hpp"
 
 std::string budget::generate_guid(){
+#ifdef _WIN32
+    UUID uuid;
+    UuidCreate(&uuid);
+    char *uuid_string;
+    UuidToStringA(&uuid, (RPC_CSTR*)&uuid_string);
+    std::string ret(uuid_string);
+    RpcStringFreeA((RPC_CSTR*)&uuid_string);
+    return ret;
+#else
     uuid_t uuid;
     char uuid_string[37];
 
     uuid_generate(uuid);
     uuid_unparse_upper(uuid, uuid_string);
-
+#endif
     return std::string(uuid_string);
 }
