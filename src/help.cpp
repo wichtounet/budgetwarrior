5f862502dbbdf65fdf90e56a0b9d7481356d3f51
Baptiste Wicht <baptiste.wicht@gmail.com>
2017-09-14 19:24:43 +0200

    Cleanup the help

--- src/help.cpp
+++ src/help.cpp
@@ -10,74 +10,75 @@
 #include "help.hpp"
 
 void budget::help_module::handle(const std::vector<std::string>&){
-    std::cout << "Usage: budget                                                     Display the overview of the current month" << std::endl << std::endl;
+    std::cout << "Usage: budget                                          Display the overview of the current month\n\n";
 
-    std::cout << "       budget account [show]                                      Show the current accounts" << std::endl;
-    std::cout << "       budget account all                                         Show all the accounts" << std::endl;
-    std::cout << "       budget account add                                         Add a new account" << std::endl;
-    std::cout << "       budget account delete (id)                                 Delete the given account" << std::endl;
-    std::cout << "       budget account edit (id)                                   Edit the given account" << std::endl;
-    std::cout << "       budget account transfer                                    Transfer money from one account to another" << std::endl << std::endl;
-    std::cout << "       budget account migrate                                     Merge two accounts together" << std::endl << std::endl;
-    std::cout << "       budget account archive                                     Archive the current accounts to start new accounts" << std::endl << std::endl;
+    std::cout << "       budget account [show]                           Show the current accounts\n";
+    std::cout << "       budget account all                              Show all the accounts\n";
+    std::cout << "       budget account add                              Add a new account\n";
+    std::cout << "       budget account delete (id)                      Delete the given account\n";
+    std::cout << "       budget account edit (id)                        Edit the given account\n";
+    std::cout << "       budget account transfer                         Transfer money from one account to another\n\n";
+    std::cout << "       budget account migrate                          Merge two accounts together\n\n";
+    std::cout << "       budget account archive                          Archive the current accounts to start new accounts\n\n";
 
-    std::cout << "       budget expense [show]                                      Display the expenses of the current month" << std::endl;
-    std::cout << "       budget expense show (month) (year)                         Display the expenses of the specified month of the specified year" << std::endl;
-    std::cout << "       budget expense all                                         Display all the expenses" << std::endl;
-    std::cout << "       budget expense add                                         Add a new expense" << std::endl;
-    std::cout << "       budget expense add (template name)                         Add a new expense from a template or create a new template" << std::endl;
-    std::cout << "       budget expense delete (id)                                 Remove completely the expense with the given id" << std::endl;
-    std::cout << "       budget expense edit (id)                                   Modify the expense with the given id" << std::endl << std::endl;
-    std::cout << "       budget expense template                                    Display the templates" << std::endl << std::endl;
+    std::cout << "       budget expense [show]                           Display the expenses of the current month\n";
+    std::cout << "       budget expense show (month) (year)              Display the expenses of the specified month of the specified year\n";
+    std::cout << "       budget expense all                              Display all the expenses\n";
+    std::cout << "       budget expense add                              Add a new expense\n";
+    std::cout << "       budget expense add (template name)              Add a new expense from a template or create a new template\n";
+    std::cout << "       budget expense delete (id)                      Remove completely the expense with the given id\n";
+    std::cout << "       budget expense edit (id)                        Modify the expense with the given id\n\n";
+    std::cout << "       budget expense template                         Display the templates\n\n";
 
-    std::cout << "       budget earning [earnings]                                  Display the earnings of the current month" << std::endl;
-    std::cout << "       budget earning show (month) (year)                         Display the earnings of the specified month of the specified year" << std::endl;
-    std::cout << "       budget earning all                                         Display all the earnings" << std::endl;
-    std::cout << "       budget earning add                                         Add a new earning" << std::endl;
-    std::cout << "       budget earning delete (id)                                 Remove completely the earning with the given id" << std::endl;
-    std::cout << "       budget earning edit (id)                                   Modify the earning with the given id" << std::endl << std::endl;
+    std::cout << "       budget earning [earnings]                       Display the earnings of the current month\n";
+    std::cout << "       budget earning show (month) (year)              Display the earnings of the specified month of the specified year\n";
+    std::cout << "       budget earning all                              Display all the earnings\n";
+    std::cout << "       budget earning add                              Add a new earning\n";
+    std::cout << "       budget earning delete (id)                      Remove completely the earning with the given id\n";
+    std::cout << "       budget earning edit (id)                        Modify the earning with the given id\n\n";
 
-    std::cout << "       budget overview [month]                                    Display the overvew of the current month" << std::endl;
-    std::cout << "       budget overview month (month) (year)                       Display the overvew of the specified month of the current year" << std::endl;
-    std::cout << "       budget overview year (year)                                Display the overvew of the specified year" << std::endl;
-    std::cout << "       budget overview aggregate                                  Display the aggregated expenses for the current year" << std::endl;
-    std::cout << "       budget overview aggregate year (year)                      Display the aggregated expenses for the given year" << std::endl << std::endl;
+    std::cout << "       budget overview [month]                         Display the overvew of the current month\n";
+    std::cout << "       budget overview month (month) (year)            Display the overvew of the specified month of the current year\n";
+    std::cout << "       budget overview year (year)                     Display the overvew of the specified year\n";
+    std::cout << "       budget overview aggregate                       Display the aggregated expenses for the current year\n";
+    std::cout << "       budget overview aggregate year (year)           Display the aggregated expenses for the given year\n\n";
 
-    std::cout << "       budget recurring [show]                                    Display the recurring expenses" << std::endl;
-    std::cout << "       budget recurring add                                       Add a new recurring expense" << std::endl;
-    std::cout << "       budget recurring delete (id)                               Remove completely the recurring expense with the given id" << std::endl;
-    std::cout << "       budget recurring edit (id)                                 Modify the recurring expense with the given id" << std::endl << std::endl;
+    std::cout << "       budget recurring [show]                         Display the recurring expenses\n";
+    std::cout << "       budget recurring add                            Add a new recurring expense\n";
+    std::cout << "       budget recurring delete (id)                    Remove completely the recurring expense with the given id\n";
+    std::cout << "       budget recurring edit (id)                      Modify the recurring expense with the given id\n\n";
 
-    std::cout << "       budget fortune [status]                                    Display the status of the fortune checks" << std::endl;
-    std::cout << "       budget fortune list                                        Display the list of all the fortune checks" << std::endl;
-    std::cout << "       budget fortune check                                       Create a new fortune check" << std::endl;
-    std::cout << "       budget fortune edit (id)                                   Edit a fortune check" << std::endl;
-    std::cout << "       budget fortune delete (id)                                 Delete a fortune check" << std::endl << std::endl;
+    std::cout << "       budget fortune [status]                         Display the status of the fortune checks\n";
+    std::cout << "       budget fortune list                             Display the list of all the fortune checks\n";
+    std::cout << "       budget fortune check                            Create a new fortune check\n";
+    std::cout << "       budget fortune edit (id)                        Edit a fortune check\n";
+    std::cout << "       budget fortune delete (id)                      Delete a fortune check\n\n";
 
-    std::cout << "       budget objective [status]                                  Show the status of the objectives for the current year" << std::endl;
-    std::cout << "       budget objective list                                      List all the defined objectives" << std::endl;
-    std::cout << "       budget objective add                                       Add a new objective" << std::endl;
-    std::cout << "       budget objective edit (id)                                 Edit an objective" << std::endl;
-    std::cout << "       budget objective delete (id)                               Delete an objective" << std::endl;
+    std::cout << "       budget objective [status]                       Show the status of the objectives for the current year\n";
+    std::cout << "       budget objective list                           List all the defined objectives\n";
+    std::cout << "       budget objective add                            Add a new objective\n";
+    std::cout << "       budget objective edit (id)                      Edit an objective\n";
+    std::cout << "       budget objective delete (id)                    Delete an objective\n";
 
-    std::cout << "       budget wish [status]                                       Show the status of the wishes" << std::endl;
-    std::cout << "       budget wish list                                           List all the wishes" << std::endl;
-    std::cout << "       budget wish estimate                                       Estimate the best date to buy something" << std::endl;
-    std::cout << "       budget wish add                                            Add a new wish" << std::endl;
-    std::cout << "       budget wish edit (id)                                      Edit a wish" << std::endl;
-    std::cout << "       budget wish delete (id)                                    Delete a wish" << std::endl;
+    std::cout << "       budget wish [status]                            Show the status of the wishes\n";
+    std::cout << "       budget wish list                                List all the wishes\n";
+    std::cout << "       budget wish estimate                            Estimate the best date to buy something\n";
+    std::cout << "       budget wish add                                 Add a new wish\n";
+    std::cout << "       budget wish edit (id)                           Edit a wish\n";
+    std::cout << "       budget wish delete (id)                         Delete a wish\n";
 
-    std::cout << "       budget debt [list]                                         Display the unpaid debts" << std::endl;
-    std::cout << "       budget debt all                                            Display all debts" << std::endl;
-    std::cout << "       budget debt paid (id)                                      Mark the given debt as paid" << std::endl;
-    std::cout << "       budget debt delete (id)                                    Delete the given debt" << std::endl;
-    std::cout << "       budget debt edit (id)                                      Edit the given debt" << std::endl << std::endl;
-    
-    std::cout << "       budget versioning save                                     Commit the budget directory changes with Git" << std::endl;
-    std::cout << "       budget versioning sync                                     Pull the remote changes on the budget directory with Git and push" << std::endl;
+    std::cout << "       budget debt [list]                              Display the unpaid debts\n";
+    std::cout << "       budget debt all                                 Display all debts\n";
+    std::cout << "       budget debt paid (id)                           Mark the given debt as paid\n";
+    std::cout << "       budget debt delete (id)                         Delete the given debt\n";
+    std::cout << "       budget debt edit (id)                           Edit the given debt\n\n";
 
-    std::cout << "       budget report [monthly]                                    Display monthly report in form of bar plot" << std::endl;
-    std::cout << "       budget report account [monthly]                            Display monthly report of a specific account in form of bar plot" << std::endl;
+    std::cout << "       budget versioning save                          Commit the budget directory changes with Git\n";
+    std::cout << "       budget versioning sync                          Pull the remote changes on the budget directory with Git and push\n\n";
+    std::cout << "       budget sync                                     Pull the remote changes on the budget directory with Git and push\n\n";
 
-    std::cout << "       budget gc                                                  Make sure all IDs are contiguous" << std::endl;
+    std::cout << "       budget report [monthly]                         Display monthly report in form of bar plot\n";
+    std::cout << "       budget report account [monthly]                 Display monthly report of a specific account in form of bar plot\n\n";
+
+    std::cout << "       budget gc                                       Make sure all IDs are contiguous\n";
 }
