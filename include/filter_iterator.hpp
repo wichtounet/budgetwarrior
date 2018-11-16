89d99e49ab2e718a1bccea3591f0511eb3224d94
Baptiste Wicht <bwicht@verisign.com>
2018-11-16 14:31:14 +0100

    filter_iterator / filter_view utility

---
+++ include/filter_iterator.hpp
@@ -0,0 +1,78 @@
+//=======================================================================
+// Copyright (c) 2013-2018 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+namespace budget {
+
+template <typename Iterator, typename Filter>
+struct filter_iterator {
+    filter_iterator(Iterator first, Iterator last, Filter filter)
+            : first(first), last(last), filter(filter) {
+        while(!this->filter(*this->first) && this->first != this->last){
+            ++this->first;
+        }
+    }
+
+    filter_iterator& operator++() {
+        if (first == last) {
+            return *this;
+        }
+
+        do {
+            ++first;
+        } while (first != last && !filter(*first));
+
+        return *this;
+    }
+
+    bool operator==(const filter_iterator& rhs) {
+        return first == rhs.first;
+    }
+
+    bool operator!=(const filter_iterator& rhs) {
+        return first != rhs.first;
+    }
+
+    decltype(auto) operator*() {
+        return *first;
+    }
+
+    decltype(auto) operator*() const {
+        return *first;
+    }
+
+private:
+    Iterator first;
+    Iterator last;
+    Filter filter;
+};
+
+template<typename Iterator, typename Filter>
+struct filter_view {
+    filter_view(Iterator first, Iterator last, Filter filter) : first(first), last(last), filter(filter) {}
+
+    auto begin(){
+        return filter_iterator<Iterator, Filter>(first, last, filter);
+    }
+
+    auto end(){
+        return filter_iterator<Iterator, Filter>(last, last, filter);
+    }
+
+private:
+    Iterator first;
+    Iterator last;
+    Filter filter;
+};
+
+template <typename Iterator, typename Filter>
+auto make_filter_view(Iterator first, Iterator last, Filter filter){
+    return filter_view<Iterator, Filter>(first, last, filter);
+}
+
+} //end of namespace budget
