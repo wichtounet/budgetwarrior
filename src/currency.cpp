c4473ff5de34fd258f4b25ded5ff7ff1e7149621
Baptiste Wicht <bwicht@verisign.com>
2019-01-03 13:25:16 +0100

    Use tuple instead of pair

--- src/currency.cpp
+++ src/currency.cpp
@@ -5,7 +5,7 @@
 //  http://opensource.org/licenses/MIT)
 //=======================================================================
 
-#include <map>
+#include <tuple>
 #include <utility>
 #include <iostream>
 
@@ -15,7 +15,7 @@
 
 namespace {
 
-std::map<std::pair<std::string, std::string>, double> exchanges;
+std::map<std::tuple<std::string, std::string>, double> exchanges;
 
 // V1 is using free.currencyconverterapi.com
 double get_rate_v1(const std::string& from, const std::string& to){
@@ -62,8 +62,8 @@ double budget::exchange_rate(const std::string& from, const std::string& to){
     if(from == to){
         return 1.0;
     } else {
-        auto key = std::make_pair(from, to);
-        auto reverse_key = std::make_pair(to, from);
+        auto key = std::make_tuple(from, to);
+        auto reverse_key = std::make_tuple(to, from);
 
         if (!exchanges.count(key)) {
             auto rate = get_rate_v1(from, to);
