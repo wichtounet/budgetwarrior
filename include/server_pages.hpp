22e4a607e1fb13b55918671ecd8a99105613cbb1
Baptiste Wicht <bwicht@verisign.com>
2019-03-20 13:54:50 +0100

    Move net worth pages out of server pages

--- include/server_pages.hpp
+++ include/server_pages.hpp
@@ -150,4 +150,18 @@ void edit_asset_values_page(const httplib::Request& req, httplib::Response& res)
 void full_batch_asset_values_page(const httplib::Request& req, httplib::Response& res);
 void current_batch_asset_values_page(const httplib::Request& req, httplib::Response& res);
 
+// Net Worth Pages
+void assets_card(budget::html_writer& w);
+void net_worth_graph(budget::html_writer& w, const std::string style = "", bool card = false);
+void net_worth_status_page(const httplib::Request& req, httplib::Response& res);
+void net_worth_small_status_page(const httplib::Request& req, httplib::Response& res);
+void net_worth_graph_page(const httplib::Request& req, httplib::Response& res);
+void net_worth_allocation_page(const httplib::Request& req, httplib::Response& res);
+void portfolio_allocation_page(const httplib::Request& req, httplib::Response& res);
+void net_worth_currency_page(const httplib::Request& req, httplib::Response& res);
+void portfolio_status_page(const httplib::Request& req, httplib::Response& res);
+void portfolio_currency_page(const httplib::Request& req, httplib::Response& res);
+void portfolio_graph_page(const httplib::Request& req, httplib::Response& res);
+void rebalance_page(const httplib::Request& req, httplib::Response& res);
+
 } //end of namespace budget
