b69b2d71dda0428dab027ac54035d1176f142cd6
Baptiste Wicht <bwicht@verisign.com>
2019-04-02 09:21:53 +0200

    Extract objectives API

---
+++ src/api/objectives_api.cpp
@@ -0,0 +1,107 @@
+//=======================================================================
+// Copyright (c) 2013-2018 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <set>
+
+#include "api/server_api.hpp"
+#include "api/objectives_api.hpp"
+
+#include "objectives.hpp"
+#include "guid.hpp"
+#include "http.hpp"
+
+using namespace budget;
+
+void budget::add_objectives_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!parameters_present(req, {"input_name", "input_type", "input_type", "input_source", "input_operator", "input_amount"})) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    objective objective;
+    objective.guid   = budget::generate_guid();
+    objective.name   = req.get_param_value("input_name");
+    objective.type   = req.get_param_value("input_type");
+    objective.source = req.get_param_value("input_source");
+    objective.op     = req.get_param_value("input_operator");
+    objective.amount = budget::parse_money(req.get_param_value("input_amount"));
+    objective.date   = budget::local_day();
+
+    add_objective(std::move(objective));
+
+    api_success(req, res, "objective " + to_string(objective.id) + " has been created", to_string(objective.id));
+}
+
+void budget::edit_objectives_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!parameters_present(req, {"input_id", "input_name", "input_type", "input_type", "input_source", "input_operator", "input_amount"})) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    auto id = req.get_param_value("input_id");
+
+    if (!budget::objective_exists(budget::to_number<size_t>(id))) {
+        api_error(req, res, "objective " + id + " does not exist");
+        return;
+    }
+
+    objective& objective = objective_get(budget::to_number<size_t>(id));
+    objective.name       = req.get_param_value("input_name");
+    objective.type       = req.get_param_value("input_type");
+    objective.source     = req.get_param_value("input_source");
+    objective.op         = req.get_param_value("input_operator");
+    objective.amount     = budget::parse_money(req.get_param_value("input_amount"));
+
+    set_objectives_changed();
+
+    api_success(req, res, "objective " + to_string(objective.id) + " has been modified");
+}
+
+void budget::delete_objectives_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!parameters_present(req, {"input_id"})) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    auto id = req.get_param_value("input_id");
+
+    if (!budget::objective_exists(budget::to_number<size_t>(id))) {
+        api_error(req, res, "The objective " + id + " does not exit");
+        return;
+    }
+
+    budget::objective_delete(budget::to_number<size_t>(id));
+
+    api_success(req, res, "objective " + id + " has been deleted");
+}
+
+void budget::list_objectives_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    std::stringstream ss;
+
+    for (auto& objective : all_objectives()) {
+        ss << objective;
+        ss << std::endl;
+    }
+
+    api_success_content(req, res, ss.str());
+}
--- src/api/server_api.cpp
+++ src/api/server_api.cpp
@@ -11,6 +11,7 @@
 #include "api/earnings_api.hpp"
 #include "api/expenses_api.hpp"
 #include "api/accounts_api.hpp"
+#include "api/objectives_api.hpp"
 
 #include "accounts.hpp"
 #include "assets.hpp"
@@ -83,96 +84,6 @@ void retirement_configure_api(const httplib::Request& req, httplib::Response& re
     save_config();
 
     api_success(req, res, "Retirement configuration was saved");
-}
-
-void add_objectives_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!parameters_present(req, {"input_name", "input_type", "input_type", "input_source", "input_operator", "input_amount"})) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    objective objective;
-    objective.guid   = budget::generate_guid();
-    objective.name   = req.get_param_value("input_name");
-    objective.type   = req.get_param_value("input_type");
-    objective.source = req.get_param_value("input_source");
-    objective.op     = req.get_param_value("input_operator");
-    objective.amount = budget::parse_money(req.get_param_value("input_amount"));
-    objective.date   = budget::local_day();
-
-    add_objective(std::move(objective));
-
-    api_success(req, res, "objective " + to_string(objective.id) + " has been created", to_string(objective.id));
-}
-
-void edit_objectives_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!parameters_present(req, {"input_id", "input_name", "input_type", "input_type", "input_source", "input_operator", "input_amount"})) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    auto id = req.get_param_value("input_id");
-
-    if (!budget::objective_exists(budget::to_number<size_t>(id))) {
-        api_error(req, res, "objective " + id + " does not exist");
-        return;
-    }
-
-    objective& objective = objective_get(budget::to_number<size_t>(id));
-    objective.name       = req.get_param_value("input_name");
-    objective.type       = req.get_param_value("input_type");
-    objective.source     = req.get_param_value("input_source");
-    objective.op         = req.get_param_value("input_operator");
-    objective.amount     = budget::parse_money(req.get_param_value("input_amount"));
-
-    set_objectives_changed();
-
-    api_success(req, res, "objective " + to_string(objective.id) + " has been modified");
-}
-
-void delete_objectives_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!parameters_present(req, {"input_id"})) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    auto id = req.get_param_value("input_id");
-
-    if (!budget::objective_exists(budget::to_number<size_t>(id))) {
-        api_error(req, res, "The objective " + id + " does not exit");
-        return;
-    }
-
-    budget::objective_delete(budget::to_number<size_t>(id));
-
-    api_success(req, res, "objective " + id + " has been deleted");
-}
-
-void list_objectives_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    std::stringstream ss;
-
-    for (auto& objective : all_objectives()) {
-        ss << objective;
-        ss << std::endl;
-    }
-
-    api_success_content(req, res, ss.str());
 }
 
 void add_assets_api(const httplib::Request& req, httplib::Response& res) {
