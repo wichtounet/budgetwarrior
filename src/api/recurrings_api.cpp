516ae77fa5997a9304aa7e2d39b1e7ffb76da7f1
Baptiste Wicht <bwicht@verisign.com>
2019-04-02 09:26:51 +0200

    Extract recurrings API

---
+++ src/api/recurrings_api.cpp
@@ -0,0 +1,105 @@
+//=======================================================================
+// Copyright (c) 2013-2018 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include <set>
+
+#include "api/server_api.hpp"
+#include "api/recurrings_api.hpp"
+
+#include "recurring.hpp"
+#include "accounts.hpp"
+#include "guid.hpp"
+#include "http.hpp"
+
+using namespace budget;
+
+void budget::add_recurrings_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!req.has_param("input_name") || !req.has_param("input_amount") || !req.has_param("input_account")) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    recurring recurring;
+    recurring.guid    = budget::generate_guid();
+    recurring.account = budget::get_account(budget::to_number<size_t>(req.get_param_value("input_account"))).name;
+    recurring.name    = req.get_param_value("input_name");
+    recurring.amount  = budget::parse_money(req.get_param_value("input_amount"));
+    recurring.recurs  = "monthly";
+
+    add_recurring(std::move(recurring));
+
+    api_success(req, res, "Recurring " + to_string(recurring.id) + " has been created", to_string(recurring.id));
+}
+
+void budget::edit_recurrings_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!req.has_param("input_id") || !req.has_param("input_name") || !req.has_param("input_amount") || !req.has_param("input_account")) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    auto id = req.get_param_value("input_id");
+
+    if (!budget::recurring_exists(budget::to_number<size_t>(id))) {
+        api_error(req, res, "recurring " + id + " does not exist");
+        return;
+    }
+
+    recurring& recurring = recurring_get(budget::to_number<size_t>(id));
+    recurring.account    = budget::get_account(budget::to_number<size_t>(req.get_param_value("input_account"))).name;
+    recurring.name       = req.get_param_value("input_name");
+    recurring.amount     = budget::parse_money(req.get_param_value("input_amount"));
+
+    set_recurrings_changed();
+
+    api_success(req, res, "Recurring " + to_string(recurring.id) + " has been modified");
+}
+
+void budget::delete_recurrings_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    if (!req.has_param("input_id")) {
+        api_error(req, res, "Invalid parameters");
+        return;
+    }
+
+    auto id = req.get_param_value("input_id");
+
+    if (!budget::recurring_exists(budget::to_number<size_t>(id))) {
+        api_error(req, res, "The recurring " + id + " does not exit");
+        return;
+    }
+
+    budget::recurring_delete(budget::to_number<size_t>(id));
+
+    api_success(req, res, "Recurring " + id + " has been deleted");
+}
+
+void budget::list_recurrings_api(const httplib::Request& req, httplib::Response& res) {
+    if (!api_start(req, res)) {
+        return;
+    }
+
+    std::stringstream ss;
+
+    for (auto& recurring : all_recurrings()) {
+        ss << recurring;
+        ss << std::endl;
+    }
+
+    api_success_content(req, res, ss.str());
+}
+
--- src/api/server_api.cpp
+++ src/api/server_api.cpp
@@ -12,6 +12,7 @@
 #include "api/expenses_api.hpp"
 #include "api/accounts_api.hpp"
 #include "api/objectives_api.hpp"
+#include "api/recurrings_api.hpp"
 
 #include "accounts.hpp"
 #include "assets.hpp"
@@ -311,92 +312,6 @@ void batch_asset_values_api(const httplib::Request& req, httplib::Response& res)
     }
 
     api_success(req, res, "Asset values have been updated");
-}
-
-void add_recurrings_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!req.has_param("input_name") || !req.has_param("input_amount") || !req.has_param("input_account")) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    recurring recurring;
-    recurring.guid    = budget::generate_guid();
-    recurring.account = budget::get_account(budget::to_number<size_t>(req.get_param_value("input_account"))).name;
-    recurring.name    = req.get_param_value("input_name");
-    recurring.amount  = budget::parse_money(req.get_param_value("input_amount"));
-    recurring.recurs  = "monthly";
-
-    add_recurring(std::move(recurring));
-
-    api_success(req, res, "Recurring " + to_string(recurring.id) + " has been created", to_string(recurring.id));
-}
-
-void edit_recurrings_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!req.has_param("input_id") || !req.has_param("input_name") || !req.has_param("input_amount") || !req.has_param("input_account")) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    auto id = req.get_param_value("input_id");
-
-    if (!budget::recurring_exists(budget::to_number<size_t>(id))) {
-        api_error(req, res, "recurring " + id + " does not exist");
-        return;
-    }
-
-    recurring& recurring = recurring_get(budget::to_number<size_t>(id));
-    recurring.account    = budget::get_account(budget::to_number<size_t>(req.get_param_value("input_account"))).name;
-    recurring.name       = req.get_param_value("input_name");
-    recurring.amount     = budget::parse_money(req.get_param_value("input_amount"));
-
-    set_recurrings_changed();
-
-    api_success(req, res, "Recurring " + to_string(recurring.id) + " has been modified");
-}
-
-void delete_recurrings_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    if (!req.has_param("input_id")) {
-        api_error(req, res, "Invalid parameters");
-        return;
-    }
-
-    auto id = req.get_param_value("input_id");
-
-    if (!budget::recurring_exists(budget::to_number<size_t>(id))) {
-        api_error(req, res, "The recurring " + id + " does not exit");
-        return;
-    }
-
-    budget::recurring_delete(budget::to_number<size_t>(id));
-
-    api_success(req, res, "Recurring " + id + " has been deleted");
-}
-
-void list_recurrings_api(const httplib::Request& req, httplib::Response& res) {
-    if (!api_start(req, res)) {
-        return;
-    }
-
-    std::stringstream ss;
-
-    for (auto& recurring : all_recurrings()) {
-        ss << recurring;
-        ss << std::endl;
-    }
-
-    api_success_content(req, res, ss.str());
 }
 
 void add_debts_api(const httplib::Request& req, httplib::Response& res) {
