9927820cc7cc690481e787fefa0177d15173dbd4
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-10-23 21:42:13 +0200

    Fix FI Ratio over time

--- src/pages/retirement_pages.cpp
+++ src/pages/retirement_pages.cpp
@@ -122,7 +122,7 @@ void budget::retirement_fi_ratio_over_time(const httplib::Request& req, httplib:
     while (date <= end_date) {
         auto ratio = budget::fi_ratio(date);
 
-        std::string datestr = "Date.UTC(" + std::to_string(date.year()) + "," + std::to_string(date.month().value - 1) + ", 1)";
+        std::string datestr = "Date.UTC(" + std::to_string(date.year()) + "," + std::to_string(date.month().value - 1) + ", " + std::to_string(date.day().value) + ")";
         ss << "[" << datestr << "," << budget::to_string(100 * ratio) << "],";
 
         date += days(1);
