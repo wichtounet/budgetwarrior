9405380cbc76752306e0fd44d30bc11953a961d6
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-10-24 11:38:32 +0200

    Introduce data_cache for convenient performance

---
+++ include/data_cache.hpp
@@ -0,0 +1,59 @@
+//=======================================================================
+// Copyright (c) 2013-2020 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#pragma once
+
+#include <vector>
+
+#include "earnings.hpp"
+#include "debts.hpp"
+#include "fortune.hpp"
+#include "assets.hpp"
+#include "liabilities.hpp"
+#include "recurring.hpp"
+#include "incomes.hpp"
+#include "accounts.hpp"
+#include "objectives.hpp"
+#include "expenses.hpp"
+#include "wishes.hpp"
+
+namespace budget {
+
+struct data_cache {
+    std::vector<earning> earnings();
+    std::vector<debt> debts();
+    std::vector<fortune> fortunes();
+    std::vector<asset_value> asset_values();
+    std::vector<liability> liabilities();
+    std::vector<recurring> recurrings();
+    std::vector<income> incomes();
+    std::vector<account> accounts();
+    std::vector<asset_share> asset_shares();
+    std::vector<asset_class> asset_classes();
+    std::vector<objective> objectives();
+    std::vector<expense> expenses();
+    std::vector<asset> assets();
+    std::vector<wish> wishes();
+
+private:
+    std::vector<earning> earnings_;
+    std::vector<debt> debts_;
+    std::vector<fortune> fortunes_;
+    std::vector<asset_value> asset_values_;
+    std::vector<liability> liabilities_;
+    std::vector<recurring> recurrings_;
+    std::vector<income> incomes_;
+    std::vector<account> accounts_;
+    std::vector<asset_share> asset_shares_;
+    std::vector<asset_class> asset_classes_;
+    std::vector<objective> objectives_;
+    std::vector<expense> expenses_;
+    std::vector<asset> assets_;
+    std::vector<wish> wishes_;
+};
+
+} //end of namespace budget
---
+++ src/data_cache.cpp
@@ -0,0 +1,123 @@
+//=======================================================================
+// Copyright (c) 2013-2020 Baptiste Wicht.
+// Distributed under the terms of the MIT License.
+// (See accompanying file LICENSE or copy at
+//  http://opensource.org/licenses/MIT)
+//=======================================================================
+
+#include "data_cache.hpp"
+
+using namespace budget;
+
+std::vector<earning> budget::data_cache::earnings() {
+    if (earnings_.empty()) {
+        earnings_ = all_earnings();
+    }
+
+    return earnings_;
+}
+
+std::vector<debt> data_cache::debts() {
+    if (debts_.empty()) {
+        debts_ = all_debts();
+    }
+
+    return debts_;
+}
+
+std::vector<fortune> data_cache::fortunes() {
+    if (fortunes_.empty()) {
+        fortunes_ = all_fortunes();
+    }
+
+    return fortunes_;
+}
+
+std::vector<asset_value> data_cache::asset_values() {
+    if (asset_values_.empty()) {
+        asset_values_ = all_asset_values();
+    }
+
+    return asset_values_;
+}
+
+std::vector<liability> data_cache::liabilities() {
+    if (liabilities_.empty()) {
+        liabilities_ = all_liabilities();
+    }
+
+    return liabilities_;
+}
+
+std::vector<recurring> data_cache::recurrings() {
+    if (recurrings_.empty()) {
+        recurrings_ = all_recurrings();
+    }
+
+    return recurrings_;
+}
+
+std::vector<income> data_cache::incomes() {
+    if (incomes_.empty()) {
+        incomes_ = all_incomes();
+    }
+
+    return incomes_;
+}
+
+std::vector<account> data_cache::accounts() {
+    if (accounts_.empty()) {
+        accounts_ = all_accounts();
+    }
+
+    return accounts_;
+}
+
+std::vector<asset_share> data_cache::asset_shares() {
+    if (asset_shares_.empty()) {
+        asset_shares_ = all_asset_shares();
+    }
+
+    return asset_shares_;
+}
+
+std::vector<asset_class> data_cache::asset_classes() {
+    if (asset_classes_.empty()) {
+        asset_classes_ = all_asset_classes();
+    }
+
+    return asset_classes_;
+}
+
+std::vector<objective> data_cache::objectives() {
+    if (objectives_.empty()) {
+        objectives_ = all_objectives();
+    }
+
+    return objectives_;
+}
+
+std::vector<expense> data_cache::expenses() {
+    if (expenses_.empty()) {
+        expenses_ = all_expenses();
+    }
+
+    return expenses_;
+}
+
+std::vector<asset> data_cache::assets() {
+    if (assets_.empty()) {
+        assets_ = all_assets();
+    }
+
+    return assets_;
+}
+
+std::vector<wish> data_cache::wishes() {
+    if (wishes_.empty()) {
+        wishes_ = all_wishes();
+    }
+
+    return wishes_;
+}
+
