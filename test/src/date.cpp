6c42e6f085b0e5cc66e85b4a67202104a13e920f
Baptiste Wicht <baptiste.wicht@gmail.com>
2020-11-01 10:52:49 +0100

    Draft plan for date unit tests

--- test/src/date.cpp
+++ test/src/date.cpp
@@ -8,6 +8,31 @@
 #include "test.hpp"
 #include "date.hpp"
 
+using namespace std::string_literals;
+
+TEST_CASE("date/to_string") {
+    budget::date a(1988, 4, 9);
+
+    FAST_CHECK_EQ(budget::date_to_string(a), "1988-04-09"s);
+    FAST_CHECK_EQ(budget::to_string(a), "1988-04-09"s);
+
+    budget::date b(2111, 10, 10);
+
+    FAST_CHECK_EQ(budget::date_to_string(b), "2111-10-10"s);
+    FAST_CHECK_EQ(budget::to_string(b), "2111-10-10"s);
+}
+
+TEST_CASE("date/from_string") {
+    auto as = budget::from_string("1988-04-09");
+    auto bs = budget::from_string("2111-10-10");
+
+    budget::date a(1988, 4, 9);
+    budget::date b(2111, 10, 10);
+
+    FAST_CHECK_EQ(a, as);
+    FAST_CHECK_EQ(b, bs);
+}
+
 TEST_CASE("date/minus/days") {
     budget::date a(2010, 5, 6);
     a -= budget::days(1);
@@ -15,4 +40,50 @@ TEST_CASE("date/minus/days") {
     FAST_CHECK_EQ(a.year(), 2010);
     FAST_CHECK_EQ(a.month(), 5);
     FAST_CHECK_EQ(a.day(), 5);
+
+    a -= budget::days(10);
+
+    FAST_CHECK_EQ(a.year(), 2010);
+    FAST_CHECK_EQ(a.month(), 4);
+    FAST_CHECK_EQ(a.day(), 25);
 }
+
+TEST_CASE("date/minus/month") {
+    budget::date a(2010, 5, 6);
+    a -= budget::months(1);
+
+    FAST_CHECK_EQ(a.year(), 2010);
+    FAST_CHECK_EQ(a.month(), 4);
+    FAST_CHECK_EQ(a.day(), 6);
+
+    a -= budget::months(10);
+
+    FAST_CHECK_EQ(a.year(), 2009);
+    FAST_CHECK_EQ(a.month(), 6);
+    FAST_CHECK_EQ(a.day(), 6);
+}
+
+TEST_CASE("date/minus/years") {
+    budget::date a(2010, 5, 6);
+    a -= budget::years(1);
+
+    FAST_CHECK_EQ(a.year(), 2009);
+    FAST_CHECK_EQ(a.month(), 5);
+    FAST_CHECK_EQ(a.day(), 6);
+
+    a -= budget::years(10);
+
+    FAST_CHECK_EQ(a.year(), 1999);
+    FAST_CHECK_EQ(a.month(), 5);
+    FAST_CHECK_EQ(a.day(), 6);
+}
+
+// TODO Test for plus
+// TODO Test for leap years
+// TODO Test for start_of_week
+// TODO Test for start_of_month
+// TODO Test for week()
+// TODO Test for year_days()
+// TODO Test for day_of_the_week
+// TODO Test for comparisons
+// TODO Test for date differences
