77afd6d421db57e06a2b77b7d1e16ff26fa6f44d
Robert C. Romano <robromano@users.noreply.github.com>
2022-08-03 16:37:36 -0700

    Add HAS_MEM_* macros which were removed from cpp_utils but still used in budgetwarrior.

--- include/utils.hpp
+++ include/utils.hpp
@@ -15,6 +15,30 @@
 #include <cctype>
 #include <locale>
 #include <iomanip>
+#include <utility>
+
+#define HAS_MEM_FUNC(func, name)                                              \
+template<typename T, typename Sign>                                           \
+struct name {                                                                 \
+    typedef char yes[1];                                                      \
+    typedef char no [2];                                                      \
+    template <typename U, U> struct type_check;                               \
+    template <typename _1> static yes &chk(type_check<Sign, &_1::func> *);    \
+    template <typename   > static no  &chk(...);                              \
+    static bool constexpr const value = sizeof(chk<T>(0)) == sizeof(yes);     \
+}
+
+#define HAS_STATIC_FIELD(field, name)                                                       \
+template <class T>                                                                          \
+class name {                                                                                \
+    template<typename U, typename =                                                         \
+        typename std::enable_if<!std::is_member_pointer<decltype(&U::field)>::value>::type> \
+    static std::true_type check(int);                                                       \
+    template <typename>                                                                     \
+    static std::false_type check(...);                                                      \
+public:                                                                                     \
+    static constexpr const bool value = decltype(check<T>(0))::value;                       \
+};	
 
 namespace budget {
 
