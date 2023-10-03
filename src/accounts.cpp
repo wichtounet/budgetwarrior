581cbe73cc490a2ca29dfae90875d429d635a35f
Baptiste Wicht <baptiste.wicht@gmail.com>
2023-10-03 07:25:11 +0200

    Refactor

--- src/accounts.cpp
+++ src/accounts.cpp
@@ -37,6 +37,65 @@ size_t get_account_id(std::string_view name, budget::year year, budget::month mo
     return 0;
 }
 
+void migrate_account(const std::string& source_account_name, const std::string& destination_account_name) {
+    // Make sure that we find the destination for
+    // each source accounts
+
+    for (const auto& account : accounts.data() | filter_by_name(source_account_name)) {
+        auto destination_id = get_account_id(destination_account_name, account.since.year(), account.since.month());
+
+        if (!destination_id) {
+            std::cout << "Impossible to find the corresponding account for account " << account.id
+                      << ". This may come from a migration issue. Open an issue on Github if you think that this is a bug" << std::endl;
+
+            return;
+        }
+    }
+
+    std::vector<size_t> deleted;
+
+    // Perform the migration
+
+    for (auto& account : accounts.data() | filter_by_name(source_account_name)) {
+        auto source_id           = account.id;
+        auto destination_account = get_account(destination_account_name, account.since.year(), account.since.month());
+        auto destination_id      = destination_account.id;
+
+        std::cout << "Migrate account " << source_id << " to account " << destination_id << std::endl;
+
+        destination_account.amount += account.amount;
+
+        for (auto& expense : all_expenses() | filter_by_account(source_id)) {
+            expense.account = destination_id;
+            indirect_edit_expense(expense, false);
+        }
+
+        for (auto& earning : all_earnings() | filter_by_account(source_id)) {
+            earning.account = destination_id;
+            indirect_edit_earning(earning, false);
+        }
+
+        accounts.indirect_edit(destination_account);
+
+        deleted.push_back(source_id);
+    }
+
+    set_expenses_changed();
+    set_earnings_changed();
+
+    // Delete the source accounts
+
+    for (auto& id : deleted) {
+        std::cout << "Delete account " << id << std::endl;
+
+        accounts.remove(id);
+    }
+
+    set_accounts_changed();
+
+    std::cout << "Migration done" << std::endl;
+}
+
 } //end of anonymous namespace
 
 std::map<std::string, std::string, std::less<>> budget::account::get_params() const {
@@ -267,62 +326,7 @@ void budget::accounts_module::handle(const std::vector<std::string>& args){
             if (source_account_name == destination_account_name) {
                 std::cout << "Migrating an account to itself has no effect" << std::endl;
             } else {
-                // Make sure that we find the destination for
-                // each source accounts
-
-                for (const auto& account : accounts.data() | filter_by_name(source_account_name)) {
-                    auto destination_id = get_account_id(destination_account_name, account.since.year(), account.since.month());
-
-                    if (!destination_id) {
-                        std::cout << "Impossible to find the corresponding account for account " << account.id
-                                  << ". This may come from a migration issue. Open an issue on Github if you think that this is a bug" << std::endl;
-
-                        return;
-                    }
-                }
-
-                std::vector<size_t> deleted;
-
-                // Perform the migration
-
-                for (auto& account : accounts.data() | filter_by_name(source_account_name)) {
-                    auto source_id           = account.id;
-                    auto destination_account = get_account(destination_account_name, account.since.year(), account.since.month());
-                    auto destination_id      = destination_account.id;
-
-                    std::cout << "Migrate account " << source_id << " to account " << destination_id << std::endl;
-
-                    destination_account.amount += account.amount;
-
-                    for (auto& expense : all_expenses() | filter_by_account(source_id)) {
-                        expense.account = destination_id;
-                        indirect_edit_expense(expense, false);
-                    }
-
-                    for (auto& earning : all_earnings() | filter_by_account(source_id)) {
-                        earning.account = destination_id;
-                        indirect_edit_earning(earning, false);
-                    }
-
-                    accounts.indirect_edit(destination_account);
-
-                    deleted.push_back(source_id);
-                }
-
-                set_expenses_changed();
-                set_earnings_changed();
-
-                // Delete the source accounts
-
-                for (auto& id : deleted) {
-                    std::cout << "Delete account " << id << std::endl;
-
-                    accounts.remove(id);
-                }
-
-                set_accounts_changed();
-
-                std::cout << "Migration done" << std::endl;
+                migrate_account(source_account_name, destination_account_name);
             }
         }
     } else if (subcommand == "archive") {
